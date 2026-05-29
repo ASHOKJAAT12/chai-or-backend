a80b17d1b838e485a5f51b00bde884e6545528b3
ASHOKJAAT12 <ajat39963@gmail.com>
2026-05-29 20:07:36 +0530

    cors or cookie-parser add

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import conectionDB from './db/index.js';
 import dotenv from 'dotenv';
+import { app } from './app.js';
 dotenv.config({
     path: './.env'
 });
 
-const app = express();
 const PORT = process.env.PORT;
 
 conectionDB()
