09cd1e0ce888ea0d539170c2cad00e4152042696
ASHOKJAAT12 <ajat39963@gmail.com>
2026-06-12 01:13:08 +0530

    add model like, comment, playlist, subscription, and tweet

---
+++ src/models/playlist.models.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const playlistSchema = new mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        video: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestemps: true
+    }
+);
+
+export const Playlist = mongoose.model("Playlist",playlistSchema);
